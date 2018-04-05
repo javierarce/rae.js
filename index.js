88287cecd17cb66fc14c1217c705bd62275d4804
Javier Arce <javierarce@users.noreply.github.com>
2018-04-05 14:27:12 +0200

    Litte refactor

--- index.js
+++ index.js
@@ -1,74 +1,73 @@
 'use strict'
 
 let fetch = require('node-fetch')
-let FormData = require('form-data')
 let xpath = require('xpath')
-let dom = require('xmldom').DOMParser
+let DOMParser = require('xmldom').DOMParser
 
-let METHOD = 'POST'
-let URL = 'http://dle.rae.es/srv/fetch'
-let PARAMS = 'TS017111a7_id=3&TS017111a7_cr=1895c885a17201dca76eb401d01fd59f:jlmn:U9YRi5sw:1485055093&TS017111a7_76=0&TS017111a7_86=0&TS017111a7_md=1&TS017111a7_rf=0&TS017111a7_ct=0&TS017111a7_pd=0'
+const URL = 'http://dle.rae.es/srv/fetch'
+const PARAMS = 'TS017111a7_id=3&TS017111a7_cr=1895c885a17201dca76eb401d01fd59f:jlmn:U9YRi5sw:1485055093&TS017111a7_76=0&TS017111a7_86=0&TS017111a7_md=1&TS017111a7_rf=0&TS017111a7_ct=0&TS017111a7_pd=0'
 
 function getDefinitionForWord (word, callback) {
-  let params = { method: METHOD, body: PARAMS }
-  let url = `${URL}?w=${word}`
+  let params = { method: 'post', body: PARAMS }
+  let url = URL + '?w=' + word
 
   let res = fetch(url, params)
     .then(body => body.text())
     .then(html => {
-    callback(extract(html))
+    callback(extractDefinitionFromHTML(html))
   })
 }
 
-function extract(text) {
+function extractDefinitionFromHTML(text) {
   let errorHandler = () => {}
 
-  const doc = new dom({ errorHandler })
+  const doc = new DOMParser({ errorHandler })
     .parseFromString(text, 'text/xml')
 
   let result = xpath.evaluate("//article", doc, null, xpath.XPathResult.ANY_TYPE, null)
 
-  let definitions = []
+  let response = []
   let node = result.iterateNext()
 
   while (node) {
     let value = node.toString().replace(/(<([^>]+)>)/ig, '')
     let definition = value.split('\n').filter(s => s)
-    let d = definition.slice(1)
+    let lines = definition.slice(1)
 
     let results = []
     let subdefinitions = {}
 
     let i = 0
 
-    while (i < d.length) {
-      let line = d[i]
+    while (i < lines.length) {
+      let line = lines[i]
 
-      let isNumber = !isNaN(+line.charAt(0))
+      let lineBeginsWithNumber = !isNaN(+line.charAt(0))
 
-      if (isNumber) {
-        line = line.match(/\d+\. (.*?)$/)[1]
-        subdefinitions.definition.push(line)
+      if (lineBeginsWithNumber) {
+        let lineWithoutNumber = line.match(/\d+\. (.*?)$/)[1]
+        subdefinitions.definition.push(lineWithoutNumber)
       } else {
-        subdefinitions = { entry: line, definition: [] }
+        subdefinitions = {
+          entry: line,
+          definition: []
+        }
+
         results.push(subdefinitions)
       }
 
       i++
     }
 
-    definitions.push({
+    response.push({
       title: definition[0],
-      definitions: results
+      definition: results
     })
 
     node = result.iterateNext()
   }
 
-  return definitions
+  return response
 }
 
-getDefinitionForWord('espejo', function (definition) {
-  console.log(definition)
-})
-
+module.exports = { getDefinitionForWord: getDefinitionForWord }
