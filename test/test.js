07603e11eb1f8440e65c7a5878616bc7bc3eec4f
Javier Arce <javierarce@users.noreply.github.com>
2018-04-05 14:27:33 +0200

    Coming soon

--- test/test.js
+++ test/test.js
@@ -1,7 +1,10 @@
 'use strict';
 
 let expect = require('chai').expect;
-let silabea = require('../index');
+let RAE = require('../index');
 
 it('should return the definition of a word', function() {
+  //RAE.getDefinitionForWord('espejo', function (definition) {
+    //console.log(definition)
+  //})
 });
